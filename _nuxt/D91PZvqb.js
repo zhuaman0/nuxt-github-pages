62bad69adba05a14bbc20bd6a2f6d808644e4d9d
zhuaman0 <aitbaevaman95@gmail.com>
2024-12-04 21:53:28 +0000

    Deploying to gh-pages from @ zhuaman0/nuxt-github-pages@c519c5c20497a74c1298479784783ac7c019635f 🚀

---
+++ _nuxt/D91PZvqb.js
@@ -0,0 +1,1 @@
+import{_ as e,t,v as c}from"./Bt-1E1cX.js";const r={};function n(o,s){return t(),c("div",null," asdf ")}const _=e(r,[["render",n]]);export{_ as default};
---
+++ _nuxt/HN1e2tPr.js
@@ -0,0 +1,1 @@
+import"./Bt-1E1cX.js";const e=""+new URL("Banner.BbZgSezF.png",import.meta.url).href;export{e as _};
